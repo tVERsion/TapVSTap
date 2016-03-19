9ba51c7990bc5fa2f94b645fca46b6b21fbe9629
Aleksei Danilov <danilow.aleksei@gmail.com>
2016-03-19 22:25:26 +0300

    Добавлена обработка мультитач

--- core/src/ru/tversion/state/PlayState.java
+++ core/src/ru/tversion/state/PlayState.java
@@ -29,7 +29,8 @@ public class PlayState extends State {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private BitmapFont font;
-    private Vector3 touchPos;
+    private Vector3 touchPosTop;
+    private Vector3 touchPosBottom;
     private Array<TapCircle> circlesTop;
     private Array<TapCircle> circlesBottom;
     private Texture background;
@@ -41,38 +42,67 @@ public class PlayState extends State {
         camera = new OrthographicCamera();
         font = new Font("material.ttf", 48, Color.BLUE, false).getFont();
         batch = new SpriteBatch();
-        touchPos = new Vector3();
+        touchPosTop = new Vector3();
+        touchPosBottom = new Vector3();
+
         circlesTop = new Array<TapCircle>();
         circlesBottom = new Array<TapCircle>();
         Gdx.input.setCatchMenuKey(true);
 
         spawnTopCircle();
+        spawnBottomCircle();
     }
 
     private void spawnTopCircle() {
-        Circle baseCircle = getCircleWithRandomXY(DIAMETER_CIRCLES);
+        Circle baseCircle = getTopCircleWithRandomXY(DIAMETER_CIRCLES);
 
         while (overlapsCircles(baseCircle)) {
-            baseCircle = getCircleWithRandomXY(DIAMETER_CIRCLES);
+            baseCircle = getTopCircleWithRandomXY(DIAMETER_CIRCLES);
         }
         int tmpRandomIndex = MathUtils.random(0, 1);
-        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex], false));
+        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
 
         while (overlapsCircles(baseCircle)) {
-            baseCircle = getCircleWithRandomXY(DIAMETER_CIRCLES);
+            baseCircle = getTopCircleWithRandomXY(DIAMETER_CIRCLES);
         }
         tmpRandomIndex = MathUtils.random(2, 3);
-        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex], false));
+        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
 
         while (overlapsCircles(baseCircle)) {
-            baseCircle = getCircleWithRandomXY(DIAMETER_CIRCLES);
+            baseCircle = getTopCircleWithRandomXY(DIAMETER_CIRCLES);
         }
         tmpRandomIndex = MathUtils.random(0, 3);
-        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex], false));
+        circlesTop.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
     }
 
-    private Circle getCircleWithRandomXY(int diameter) {
+    private void spawnBottomCircle() {
+        Circle baseCircle = getBottomCircleWithRandomXY(DIAMETER_CIRCLES);
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getBottomCircleWithRandomXY(DIAMETER_CIRCLES);
+        }
+        int tmpRandomIndex = MathUtils.random(0, 1);
+        circlesBottom.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getBottomCircleWithRandomXY(DIAMETER_CIRCLES);
+        }
+        tmpRandomIndex = MathUtils.random(2, 3);
+        circlesBottom.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getBottomCircleWithRandomXY(DIAMETER_CIRCLES);
+        }
+        tmpRandomIndex = MathUtils.random(0, 3);
+        circlesBottom.add(new TapCircle(TEXTURES_CIRCLES[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, MARKS[tmpRandomIndex]));
+    }
+
+    private Circle getTopCircleWithRandomXY(int diameter) {
         return new Circle(MathUtils.random(0, gsm.getWidth() - diameter), MathUtils.random(((gsm.getHeight() - diameter) / 2 + diameter), gsm.getHeight() - diameter), diameter / 2);
+    }
+
+    private Circle getBottomCircleWithRandomXY(int diameter) {
+        return new Circle(MathUtils.random(0, gsm.getWidth() - diameter), MathUtils.random(0, gsm.getHeight() / 2 - diameter - (int) diameter / 2), diameter / 2);
     }
 
     private boolean overlapsCircles(Circle circle) {
@@ -81,26 +111,72 @@ public class PlayState extends State {
                 return true;
             }
         }
+        for (int i = 0; i < circlesBottom.size; i++) {
+            if (circle.overlaps(circlesBottom.get(i))) {
+                return true;
+            }
+        }
         return false;
     }
 
-    private void deleteCircles() {
+    private void deleteTopCircles() {
         for (int i = 0; i < NUMBER_CIRCLES; i++) {
             circlesTop.removeIndex(0);
         }
     }
 
+    private void deleteBottomCircles() {
+        for (int i = 0; i < NUMBER_CIRCLES; i++) {
+            circlesBottom.removeIndex(0);
+        }
+    }
+
     @Override
     protected void handleInput() {
-        if (Gdx.input.justTouched()) {
-            touchPos.set(Gdx.input.getX(), gsm.getHeight() - Gdx.input.getY(), 0);
-            for (int i = 0; i < NUMBER_CIRCLES; i++) {
-                if (circlesTop.get(i).contains(touchPos.x - circlesTop.get(i).getTexture().getWidth() / 2, touchPos.y - circlesTop.get(i).getTexture().getHeight() / 2)) {
-                    deleteCircles();
-                    spawnTopCircle();
-                    break;
+        if (Gdx.input.isTouched(0)) {
+
+            touchPosTop.set(Gdx.input.getX(0), gsm.getHeight() - Gdx.input.getY(0), 0);
+            if (gsm.getHeight() - Gdx.input.getY(0) > gsm.getHeight() / 2) {
+                for (int i = 0; i < NUMBER_CIRCLES; i++) {
+                    if (circlesTop.get(i).contains(touchPosTop.x - circlesTop.get(i).getTexture().getWidth() / 2, touchPosTop.y - circlesTop.get(i).getTexture().getHeight() / 2)) {
+                        deleteTopCircles();
+                        spawnTopCircle();
+                        break;
+                    }
+                }
+            } else {
+                for (int i = 0; i < NUMBER_CIRCLES; i++) {
+                    if (circlesBottom.get(i).contains(touchPosTop.x - circlesBottom.get(i).getTexture().getWidth() / 2, touchPosTop.y - circlesBottom.get(i).getTexture().getHeight() / 2)) {
+                        deleteBottomCircles();
+                        spawnBottomCircle();
+                        break;
+                    }
                 }
             }
+
+        }
+
+        if (Gdx.input.isTouched(1)) {
+
+            touchPosBottom.set(Gdx.input.getX(1), gsm.getHeight() - Gdx.input.getY(1), 0);
+            if (gsm.getHeight() - Gdx.input.getY(1) > gsm.getHeight() / 2) {
+                for (int i = 0; i < NUMBER_CIRCLES; i++) {
+                    if (circlesTop.get(i).contains(touchPosBottom.x - circlesTop.get(i).getTexture().getWidth() / 2, touchPosBottom.y - circlesTop.get(i).getTexture().getHeight() / 2)) {
+                        deleteTopCircles();
+                        spawnTopCircle();
+                        break;
+                    }
+                }
+            } else {
+                for (int i = 0; i < NUMBER_CIRCLES; i++) {
+                    if (circlesBottom.get(i).contains(touchPosBottom.x - circlesBottom.get(i).getTexture().getWidth() / 2, touchPosBottom.y - circlesBottom.get(i).getTexture().getHeight() / 2)) {
+                        deleteBottomCircles();
+                        spawnBottomCircle();
+                        break;
+                    }
+                }
+            }
+
         }
     }
 
@@ -117,8 +193,27 @@ public class PlayState extends State {
         sb.draw(background, 0, 0, gsm.getWidth(), gsm.getHeight());
         sb.draw(timer, (gsm.getWidth() / 2) - (timer.getWidth() / 2), gsm.getHeight() / 2 - (timer.getHeight() / 2));
 
+        for (int i = 0; i < circlesBottom.size; i++) {
+            sb.draw(circlesBottom.get(i).getTexture(), circlesBottom.get(i).x, circlesBottom.get(i).y);
+        }
+
         for (int i = 0; i < circlesTop.size; i++) {
-            sb.draw(circlesTop.get(i).getTexture(), circlesTop.get(i).x, circlesTop.get(i).y);
+            sb.draw(circlesTop.get(i).getTexture(),
+                    circlesTop.get(i).x, //(gsm.getWidth() - circlesBottom.get(i).getTexture().getWidth()) / 2.0f,
+                    circlesTop.get(i).y, //(gsm.getHeight() - circlesBottom.get(i).getTexture().getHeight()) / 2.0f,
+                    circlesTop.get(i).getTexture().getWidth() / 2.0f,
+                    circlesTop.get(i).getTexture().getHeight() / 2.0f,
+                    circlesTop.get(i).getTexture().getWidth(),
+                    circlesTop.get(i).getTexture().getHeight(),
+                    1,
+                    1,
+                    180,
+                    0,
+                    0,
+                    circlesTop.get(i).getTexture().getHeight(),
+                    circlesTop.get(i).getTexture().getHeight(),
+                    false,
+                    false);
         }
 
         sb.end();
