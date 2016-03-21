2ce18226eea24060b2b3bdf1835e526b69713d40
Aleksei Danilov <danilow.aleksei@gmail.com>
2016-03-21 16:37:24 +0300

    Рефакторинг класса PlayState Выделение интерфейса Сircles и унаследовавших от него классов TopCircles и BottomCircles

---
+++ core/src/ru/tversion/logic2screen/TopCircles.java
@@ -0,0 +1,92 @@
+package ru.tversion.logic2screen;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+
+import ru.tversion.figures.TapCircle;
+import ru.tversion.state.GameStateManager;
+
+public class TopCircles implements Circles {
+    private Array<TapCircle> circles;
+    private GameStateManager gsm;
+    private static int NUMBER_CIRCLES = 3;
+
+    public TopCircles(GameStateManager gsm) {
+        circles = new Array<TapCircle>();
+        this.gsm = gsm;
+    }
+
+    @Override
+    public void draw(SpriteBatch sb) {
+        for (int i = 0; i < circles.size; i++) {
+            sb.draw(circles.get(i).getTexture(),
+                    circles.get(i).x,
+                    circles.get(i).y,
+                    circles.get(i).getTexture().getWidth() / 2.0f,
+                    circles.get(i).getTexture().getHeight() / 2.0f,
+                    circles.get(i).getTexture().getWidth(),
+                    circles.get(i).getTexture().getHeight(),
+                    1,
+                    1,
+                    180,
+                    0,
+                    0,
+                    circles.get(i).getTexture().getHeight(),
+                    circles.get(i).getTexture().getHeight(),
+                    false,
+                    false);
+        }
+    }
+
+    @Override
+    public void spawn(Texture[][] textures, int[] marks, int diameter) {
+        Circle baseCircle = getCircleWithRandomXY(diameter);
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getCircleWithRandomXY(diameter);
+        }
+        int tmpRandomIndex = MathUtils.random(0, 1);
+        circles.add(new TapCircle(textures[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, marks[tmpRandomIndex]));
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getCircleWithRandomXY(diameter);
+        }
+        tmpRandomIndex = MathUtils.random(2, 3);
+        circles.add(new TapCircle(textures[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, marks[tmpRandomIndex]));
+
+        while (overlapsCircles(baseCircle)) {
+            baseCircle = getCircleWithRandomXY(diameter);
+        }
+        tmpRandomIndex = MathUtils.random(0, 3);
+        circles.add(new TapCircle(textures[MathUtils.random(0, 3)][tmpRandomIndex], baseCircle, marks[tmpRandomIndex]));
+    }
+
+    @Override
+    public Circle getCircleWithRandomXY(int diameter) {
+        return new Circle(MathUtils.random(0, gsm.getWidth() - diameter), MathUtils.random(((gsm.getHeight() - diameter) / 2 + diameter), gsm.getHeight() - diameter), diameter / 2);
+
+    }
+
+    @Override
+    public boolean overlapsCircles(Circle circle) {
+        for (int i = 0; i < circles.size; i++) {
+            if (circle.overlaps(circles.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void delete() {
+        for (int i = 0; i < NUMBER_CIRCLES; i++) {
+            circles.removeIndex(0);
+        }
+    }
+    @Override
+    public TapCircle getCircle(int i) {
+        return circles.get(i);
+    }
+}
